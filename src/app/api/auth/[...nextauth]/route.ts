64b1f4f7aa7cdba1ac8029e970a1b4438edf18ca
Jaikp <jaikp000@gmail.com>
2025-03-09 01:11:49 +0530

    update

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,24 @@
-import { handlers } from "@/utils/auth" // Referring to the auth.ts we just created
-export const { GET, POST } = handlers
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/db/prisma";
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma), // Uses Prisma (Must be inside API route)
+  session: { strategy: "jwt" },
+  providers: [Google],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      session.user.id = token.id as string;
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
