6df2f0b6aca56ef9f3c8d8cc62be722eb34a13cb
Jaikp <jaikp000@gmail.com>
2025-03-19 02:41:19 +0530

    update prisma

--- src/server-actions/storeData.ts
+++ src/server-actions/storeData.ts
@@ -6,7 +6,7 @@ import { auth } from "@clerk/nextjs/server";
 export async function storeData(values : any) {
 
     const {userId} = await auth();
-
+    console.log(userId);
     const response = await prisma.monitor.create({
         data : {
             url : values.url,
